97b703e63a117512d9e3236a4beac8a149125b30
ARC-XX <76841018+ARC-XX@users.noreply.github.com>
2023-03-13 20:18:31 +0100

    Update truenasscale-backup.sh

--- truenasscale-backup.sh
+++ truenasscale-backup.sh
@@ -8,7 +8,7 @@ DATE=$(date +%Y%m%d)
 OPTIONS='{"secretseed":true,"root_authorized_keys":true}'
 
 # make the api request for config as xml and alias as json
-curl -X POST -H "Content-Type: application/json" -d "$OPTIONS" -H "Authorization: Bearer ${KEY}" "https://${HOST}/api/v2.0/config/save" -o "$PATHCONFIG"/truenasscale/nas-config-"${DATE}".tar
+curl -X POST -H "Content-Type: application/json" -d "${OPTIONS}" -H "Authorization: Bearer ${KEY}" "https://${HOST}/api/v2.0/config/save" -o "${PATHCONFIG}"/truenasscale/nas-config-"${DATE}".tar
 
 # check the backup destination for backups older then 14 days, if older delete the backup
-find "$PATHCONFIG" -type f -name "$host"-config-\*.tar -mtime +14 -delete
+find "${PATHCONFIG}" -type f -name "${HOST}"-config-\*.tar -mtime +14 -delete
